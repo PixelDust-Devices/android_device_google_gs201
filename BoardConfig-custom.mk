e98f4512a55b44127f5e5cd1a62c2e198352c22a
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-23 22:32:25 +0530

    Build vendor_dlkm
    Change-Id: Ic2a0e4cdfdcc448a73bef41e0d2140e9d0aefcf0
    Signed-off-by: spezi77 <spezi7713@gmail.com>

--- BoardConfig-custom.mk
+++ BoardConfig-custom.mk
@@ -1,5 +1,11 @@
 #
-# Copyright (C) 2023 Another KitchenSink Project
+# Copyright (C) 2023-2025 Another KitchenSink Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor_dlkm
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
