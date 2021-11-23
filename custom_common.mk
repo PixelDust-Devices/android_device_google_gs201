761e0ca6a2bafcdf056fc9a788ad37ef923f50d5
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-23 15:05:41 -0500

    Set camera extension package to Google package
    Change-Id: I519ebf8a1e2a9d06561471206c91c01b352ebd47
    Signed-off-by: spezi77 <spezi7713@gmail.com>

--- custom_common.mk
+++ custom_common.mk
@@ -5,3 +5,8 @@
 #
 
 DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-custom
+
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
