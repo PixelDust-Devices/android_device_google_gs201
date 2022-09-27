a9458a17bc249ae8574e65c2ed264f5a73c84d0f
Matt Filetto <matt.filetto@gmail.com>
2022-09-27 06:02:43 -0700

    gs101: reserve space additonal space for partitions
    * Reserved additional space for the following:
      -product
      -system
      -system_ext
      -vendor_dlkm
      -Vendor
    
    * This fixes the following issues:
    
    * When built without WITH_GAPPS=true, compiler will reserve additional
      free space to your product and system_ext.imgs to allow gapps to flash
      properly
    
    * Since StatiXOS updated kernel was released users were having issues
      flashing a custom kernel from FKM or some other userspace flasher.
      This is because StatiXOS kernel removed some bloat and results in a
      vendor_dlkm.img 10 MB smaller, so When user tries to flash any other
      kernel on top it fails with not enough space error. this is not an
      issue in fastboot , it resizes by default before flashing to size of
      the update image. By reserving space that is as much as the stock
      image we remedy this situation.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    
    # Conflicts:
    #	BoardConfig-common.mk
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

---
+++ BoardConfigSpark.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2019 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Partition attributes
+ifneq ($(WITH_GAPPS),true)
+BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 614400000
+BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 1752350720
+endif
+
