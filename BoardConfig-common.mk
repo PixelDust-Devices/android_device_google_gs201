AOSPA/android_device_google_gs101

Add initial aospa makefiles
Change-Id: I16ed0b3008a7e4aa63dec32fbfd7c196f157d3fb

BoardConfig-common.mk
@@ -419,3 +419,5 @@ BOARD_KERNEL_CMDLINE += at24.write_timeout=100
 BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 
 -include vendor/google_devices/gs101/proprietary/BoardConfigVendor.mk
+
+include device/google/gs101/BoardConfigAOSPA.mk

PixelDust-Devices/android_device_google_gs201

BoardConfig-common.mk
@@ -414,3 +414,5 @@ BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 
 # Protected VM firmware
 BOARD_PVMFWIMAGE_PARTITION_SIZE := 0x00100000
+
+include device/google/gs201/BoardConfigAOSPA.mk