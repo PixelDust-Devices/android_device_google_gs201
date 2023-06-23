8b2bb4dda6444c023efa5c8812038488b3ecc926
LuK1337 <priv.luk@gmail.com>
2023-06-23 16:07:35 +0200

    gs201: don't inherit aosp_product.mk
    This includes some unnecessary telephony bits and overrides.
    
    Change-Id: Id75d9fd584a9981e756bb65a668ed526b4d1cd05
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- aosp_common.mk
+++ aosp_common.mk
@@ -29,7 +29,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 #
 # All components inherited here go to product image
 #
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
+ifneq ($(BOARD_WITHOUT_RADIO),true)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
+endif
 
 #
 # All components inherited here go to vendor image
