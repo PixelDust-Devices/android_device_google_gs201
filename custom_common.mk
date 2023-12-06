6f9fa1f09ec6f4f3b8fcb6b5f1076a5f9d4465f4
LuK1337 <priv.luk@gmail.com>
2023-12-06 09:54:05 +0100

    Add liboemcrypto.so to vendor linker config
    This lets com.google.android.widevine.apex access it.
    
    Change-Id: I4e9654fe6317b722f5fbf43b92abb367c18640ce
    Signed-off-by: spezi77 <spezi7713@gmail.com>

--- custom_common.mk
+++ custom_common.mk
@@ -10,3 +10,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-custom
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
+# Linker config
+PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
+    device/google/gs201/linker.config.json
