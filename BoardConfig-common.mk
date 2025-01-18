arrow-raviole/device_google_gs101

Create BoardConfig-custom
Change-Id: I8136e9f0df40ca3bf96dbfd2453275481178cdaa

BoardConfig-common.mk
@@ -381,3 +381,5 @@ BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 BOARD_PVMFWIMAGE_PARTITION_SIZE := 0x00100000
 
 -include vendor/google_devices/gs101/proprietary/BoardConfigVendor.mk
+
+include device/google/gs101/BoardConfig-custom.mk

PixelDust-Devices/android_device_google_gs201

Signed-off-by: spezi77 <spezi7713@gmx.net>
Signed-off-by: spezi77 <spezi7713@gmail.com>

BoardConfig-common.mk
@@ -429,3 +429,5 @@ BOARD_PVMFWIMAGE_PARTITION_SIZE := 0x00100000
 
 # pick up library for cleaning digital car keys on factory reset
 -include vendor/google_devices/gs-common/proprietary/BoardConfigVendor.mk
+
+include device/google/gs201/BoardConfig-custom.mk